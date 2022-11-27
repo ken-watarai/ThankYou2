9ecca9420b74df0f7ca804f4d4b12952546fc0b2
ken-watarai <66110193+ken-watarai@users.noreply.github.com>
2022-11-27 09:41:20 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function OnButtonClick1() {
+  target = document.getElementById("msg_form_w");
+  target.classList.add("appear");
+}
+function OnButtonClick11() {
+  target = document.getElementById("msg_form_w");
+  target.classList.remove("appear");
+}
+function OnButtonClick2() {
+  target = document.getElementById("msg_form_m");
+  target.classList.add("appear_r");
+}
+function OnButtonClick21() {
+  target = document.getElementById("msg_form_m");
+  target.classList.remove("appear_r");
+}
