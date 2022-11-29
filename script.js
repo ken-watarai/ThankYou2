57608cf4ada04b0d4daf4fe7deda81a7d988f3c3
ken-watarai <66110193+ken-watarai@users.noreply.github.com>
2022-11-29 23:06:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 const target_l = document.getElementById("msg_form_l");
 const target_r = document.getElementById("msg_form_r");
+const circle_oldsis = document.getElementById("circle_oldsis");
+const circle_youngsis = document.getElementById("circle_youngsis");
 let flag_l = false;
 let flag_r = false;
 
 // element.classList.contains("item")
 function OnButtonClick1() {
   target_l.classList.add("appear");
+  circle_oldsis.classList.add("disappear");
 }
 function OnButtonClick11() {
   target_l.classList.remove("appear");
+  circle_oldsis.classList.remove("disappear");
 }
 function OnButtonClick2() {
   target_r.classList.add("appear_r");
+  circle_youngsis.classList.add("disappear");
 }
 function OnButtonClick21() {
   target_r.classList.remove("appear_r");
+  circle_youngsis.classList.remove("disappear");
 }
 
 
